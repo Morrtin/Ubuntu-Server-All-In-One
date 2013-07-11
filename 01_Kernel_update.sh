7235b8cdaf34e16129a07848ffdade687692d308
Morrtin <privat@thomasgruebl.at>
2013-07-11 22:41:18 +0200

    Kernel & Firmware version updated

--- 01_Kernel_update.sh
+++ 01_Kernel_update.sh
@@ -7,8 +7,8 @@
 
 FIRMWARE_DOWNLOAD_URL="http://ubuntu.mirror.cambrium.nl/ubuntu//pool/main/l/linux-firmware/"
 KERNEL_DOWNLOAD_URL="http://kernel.ubuntu.com/~kernel-ppa/mainline/"
-KERNEL_VERSION="v3.9.8-saucy"
-FIRMWARE_VERSION="v1.110"
+KERNEL_VERSION="v3.10-saucy"
+FIRMWARE_VERSION="v1.111"
 
 function download()
 {
@@ -20,11 +20,11 @@ clear
 mkdir ~/3.9.x
 cd ~/3.9.x
 echo "Downloading Firmware $FIRMWARE_VERSION"
-download $FIRMWARE_DOWNLOAD_URL"linux-firmware_1.110_all.deb"
+download $FIRMWARE_DOWNLOAD_URL"linux-firmware_1.111_all.deb"
 echo "Downloading Kernel $KERNEL_VERSION"
-download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.9.8-030908-generic_3.9.8-030908.201306271518_amd64.deb"  > /dev/null 2>&1
-download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.9.8-030908_3.9.8-030908.201306271518_all.deb"  > /dev/null 2>&1
-download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-image-3.9.8-030908-generic_3.9.8-030908.201306271518_amd64.deb"  > /dev/null 2>&1
+download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.10.0-031000-generic_3.10.0-031000.201306301935_amd64.deb"  > /dev/null 2>&1
+download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.10.0-031000_3.10.0-031000.201306301935_all.deb"  > /dev/null 2>&1
+download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-image-3.10.0-031000-generic_3.10.0-031000.201306301935_amd64.deb"  > /dev/null 2>&1
 echo "Updating computer"
 sudo dpkg -i *deb
 echo "Updating Grub"
--- batched_trim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-LOG=/var/log/batched_trim.log
-echo "*** $(date -R) ***" >> $LOG
-fstrim -v / >> $LOG
-fstrim -v /home >> $LOG
