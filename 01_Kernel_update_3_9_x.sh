f0cd90bf0c59927b30df81f46dad79ea3e7b625f
Morrtin <privat@thomasgruebl.at>
2013-07-27 06:12:44 +0200

    update kernel 3.9.11

--- 01_Kernel_update_3_9_x.sh
+++ 01_Kernel_update_3_9_x.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 # @author   Morrtin
-# @date     2013-07-11
-# @version  1.1
+# @date     2013-07-27
+# @version  1.11
 #
 
 FIRMWARE_DOWNLOAD_URL="http://ubuntu.mirror.cambrium.nl/ubuntu//pool/main/l/linux-firmware/"
 KERNEL_DOWNLOAD_URL="http://kernel.ubuntu.com/~kernel-ppa/mainline/"
-KERNEL_VERSION="v3.9.10-saucy"
+KERNEL_VERSION="v3.9.11-saucy"
 FIRMWARE_VERSION="v1.112"
 
 function download()
@@ -22,9 +22,9 @@ cd ~/3.9.x
 echo "Downloading Firmware $FIRMWARE_VERSION"
 download $FIRMWARE_DOWNLOAD_URL"linux-firmware_1.112_all.deb"
 echo "Downloading Kernel $KERNEL_VERSION"
-download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.9.10-030910-generic_3.9.10-030910.201307131608_amd64.deb"  > /dev/null 2>&1
-download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.9.10-030910_3.9.10-030910.201307131608_all.deb"  > /dev/null 2>&1
-download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-image-3.9.10-030910-generic_3.9.10-030910.201307131608_amd64.deb"  > /dev/null 2>&1
+download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.9.11-030911-generic_3.9.11-030911.201307202035_amd64.deb"  > /dev/null 2>&1
+download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.9.11-030911_3.9.11-030911.201307202035_all.deb"  > /dev/null 2>&1
+download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-image-3.9.11-030911-generic_3.9.11-030911.201307202035_amd64.deb"  > /dev/null 2>&1
 echo "Updating computer"
 sudo dpkg -i *deb
 echo "Updating Grub"
