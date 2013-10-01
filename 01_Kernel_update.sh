ed539b5a7eec4dd2e5c94d3a3460f9253f771c99
Morrtin <privat@thomasgruebl.at>
2013-10-01 20:15:16 +0200

    upd fw & kernel

--- 01_Kernel_update.sh
+++ 01_Kernel_update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
 # @author   Morrtin
-# @date     2013-09-19
-# @version  1.11
+# @date     2013-10-01
+# @version  1.12
 #
 
 FIRMWARE_DOWNLOAD_URL="http://ubuntu.mirror.cambrium.nl/ubuntu//pool/main/l/linux-firmware/"
 KERNEL_DOWNLOAD_URL="http://kernel.ubuntu.com/~kernel-ppa/mainline/"
-KERNEL_VERSION="v3.11.1-saucy"
-FIRMWARE_VERSION="v1.114"
+KERNEL_VERSION="v3.11.2-saucy"
+FIRMWARE_VERSION="v1.115"
 
 function download()
 {
@@ -20,11 +20,11 @@ clear
 mkdir ~/3.11.x
 cd ~/3.11.x
 echo "Downloading Firmware $FIRMWARE_VERSION"
-download $FIRMWARE_DOWNLOAD_URL"linux-firmware_1.114_all.deb"
+download $FIRMWARE_DOWNLOAD_URL"linux-firmware_1.115_all.deb"
 echo "Downloading Kernel $KERNEL_VERSION"
-download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.11.1-031101-generic_3.11.1-031101.201309141102_amd64.deb"
-download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.11.1-031101_3.11.1-031101.201309141102_all.deb"
-download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-image-3.11.1-031101-generic_3.11.1-031101.201309141102_amd64.deb"
+download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.11.2-031102-generic_3.11.2-031102.201309262136_amd64.deb"
+download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.11.2-031102_3.11.2-031102.201309262136_all.deb"
+download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-image-3.11.2-031102-generic_3.11.2-031102.201309262136_amd64.deb"
 echo "Updating computer"
 sudo dpkg -i *deb
 echo "Updating Grub"
