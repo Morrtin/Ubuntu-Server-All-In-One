0aa490c4759a160ddf25771c86caed99359c8fc1
Morrtin <privat@thomasgruebl.at>
2014-05-28 21:27:14 +0200

    update krnl & frm

--- 01_Kernel_update.sh
+++ 01_Kernel_update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
 # @author   Morrtin
-# @date     2014-05-12
-# @version  2.01
+# @date     2014-05-28
+# @version  2.02
 #
 
 FIRMWARE_DOWNLOAD_URL="http://ubuntu.mirror.cambrium.nl/ubuntu//pool/main/l/linux-firmware/"
 KERNEL_DOWNLOAD_URL="http://kernel.ubuntu.com/~kernel-ppa/mainline/"
-KERNEL_VERSION="v3.13.11.2-trusty"
-FIRMWARE_VERSION="v1.129"
+KERNEL_VERSION="v3.14.1-trusty"
+FIRMWARE_VERSION="v1.130"
 
 function download()
 {
@@ -20,11 +20,11 @@ clear
 mkdir ~/3.13.x
 cd ~/3.13.x
 echo "Downloading Firmware $FIRMWARE_VERSION"
-download $FIRMWARE_DOWNLOAD_URL"linux-firmware_1.129_all.deb"
+download $FIRMWARE_DOWNLOAD_URL"linux-firmware_1.130_all.deb"
 echo "Downloading Kernel $KERNEL_VERSION"
-download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.13.11-03131102-generic_3.13.11-03131102.201405081435_amd64.deb"
-download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.13.11-03131102_3.13.11-03131102.201405081435_all.deb"
-download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-image-3.13.11-03131102-generic_3.13.11-03131102.201405081435_amd64.deb"
+download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.14.1-031401-generic_3.14.1-031401.201404141220_amd64.deb"
+download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.14.1-031401_3.14.1-031401.201404141220_all.deb"
+download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-image-3.14.1-031401-generic_3.14.1-031401.201404141220_amd64.deb"
 echo "Updating computer"
 sudo dpkg -i *deb
 echo "Updating Grub"
