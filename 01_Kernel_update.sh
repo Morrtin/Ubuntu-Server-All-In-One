de51020a70012ac178772d4d42daefdba7bcf134
Morrtin <privat@thomasgruebl.at>
2015-03-07 11:38:13 +0100

    update frm

--- 01_Kernel_update.sh
+++ 01_Kernel_update.sh
@@ -4,11 +4,13 @@
 # @date     2014-05-28
 # @version  2.02
 #
+# Trusty (14.04LTS) ->	Firmware:	1.127.5
+# Utopic (14.10)	->	Firmware:	1.133
 
 FIRMWARE_DOWNLOAD_URL="http://ubuntu.mirror.cambrium.nl/ubuntu//pool/main/l/linux-firmware/"
 KERNEL_DOWNLOAD_URL="http://kernel.ubuntu.com/~kernel-ppa/mainline/"
 KERNEL_VERSION="v3.14.1-trusty"
-FIRMWARE_VERSION="v1.130"
+FIRMWARE_VERSION="v1.133"
 
 function download()
 {
@@ -20,7 +22,7 @@ clear
 mkdir ~/3.13.x
 cd ~/3.13.x
 echo "Downloading Firmware $FIRMWARE_VERSION"
-download $FIRMWARE_DOWNLOAD_URL"linux-firmware_1.130_all.deb"
+download $FIRMWARE_DOWNLOAD_URL"linux-firmware_1.133_all.deb"
 echo "Downloading Kernel $KERNEL_VERSION"
 download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.14.1-031401-generic_3.14.1-031401.201404141220_amd64.deb"
 download $KERNEL_DOWNLOAD_URL"$KERNEL_VERSION/linux-headers-3.14.1-031401_3.14.1-031401.201404141220_all.deb"
